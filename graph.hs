019ca612f1ed11fc00721696e0086787f572745b
Pablo Dávila Herrero <pablodavila314@gmail.com>
2021-12-29 14:15:20 +0100

    Add graph samples

--- graph.hs
+++ graph.hs
@@ -1,10 +1,13 @@
 
 -- Data types --
 
+-- (id, tag)
 type Vertex a = (Int, a)
 
+-- (tag, id1, id2)
 type Edge a = (a, Int, Int)
 
+-- ([vertices], [edges])
 type Graph a b = ([Vertex a], [Edge b])
 
 
@@ -16,22 +19,67 @@ g1 = (
             (0, "Vertex 0"),
             (1, "Vertex 1"),
             (2, "Vertex 2"),
-            (3, "Vertex 3")
+            (3, "Vertex 3"),
+            (4, "Vertex 4")
         ],
         [ -- Edges
             (0.5, 0, 1),
             (2.2, 1, 2),
             (1.3, 2, 3),
-            (3.7, 3, 0)
+            (3.7, 3, 1),
+            (3.2, 2, 4)
         ]
     )
 
-{- Dibujo g1:
+g2 :: (Graph String Float)
+g2 = (
+        [ -- Vertices
+            (5, "Vertex 5"),
+            (6, "Vertex 6"),
+            (7, "Vertex 7"),
+            (8, "Vertex 8"),
+            (9, "Vertex 9")
+        ],
+        [ -- Edges
+            (0.5, 5, 6),
+            (2.2, 7, 6),
+            (1.3, 7, 8),
+            (3.7, 8, 6),
+            (3.2, 7, 9)
+        ]
+    )
 
+g3 = g1 +* g2
+
+g4 :: (Graph String Float)
+g4 = (
+        [ -- Vertices
+            (0, "Vertex 0"),
+            (1, "Vertex 1"),
+            (2, "Vertex 2")
+        ],
+        [ -- Edges
+            (0.5, 0, 1),
+            (2.2, 1, 2),
+            (1.3, 2, 0)
+        ]
+    )
+
+v0 = head $ fst g1
+v5 = head $ fst g2
+
+{-
+Drawing g1:
  v0 -> v1
      _^ |
    _/   v
- v3 <- v2
+ v3 <- v2 -> v4
+
+Drawing g2:
+ v5 -> v6
+     _^ ^
+   _/   |
+ v8 <- v7 -> v9
 -}
 
 
