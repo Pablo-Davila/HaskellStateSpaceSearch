f0c274294b63b4ee603dd268cca7740ceb621a02
Pablo Dávila Herrero <Pablo-Davila@users.noreply.github.com>
2022-01-20 16:00:43 +0100

    Add Graph.Samples tests

---
+++ graphTests.hs
@@ -0,0 +1,15 @@
+
+import Graph
+import Graph.Samples
+import Graph.Connection
+import Graph.TreeCover
+
+
+test = and [
+
+        -- Graph.Samples tests
+        checkGraphIds g1,
+        checkGraphIds g2,
+        checkGraphIds g3,
+        checkGraphIds g4
+    ]
