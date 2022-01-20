58c0929a3a7ce2985bc552ee24afb90f0de6f8a2
Pablo Dávila Herrero <Pablo-Davila@users.noreply.github.com>
2022-01-20 17:27:52 +0100

    Add Graph.TreeCover tests

--- graphTests.hs
+++ graphTests.hs
@@ -5,18 +5,19 @@ import Graph.Connection
 import Graph.TreeCover
 
 
-tests = [
-        -- Graph tests
+testsGraph = [  -- Graph tests
         g1 +* g2 -* g2 == g1,
-        g1 +* g2 -* g1 == g2,
+        g1 +* g2 -* g1 == g2
+    ]
 
-        -- Graph.Samples tests
+testsSamples = [  -- Graph.Samples tests
         checkGraphIds g1,
         checkGraphIds g2,
         checkGraphIds g3,
-        checkGraphIds g4,
+        checkGraphIds g4
+    ]
 
-        -- Graph.Connection tests
+testsConnection = [  -- Graph.Connection tests
         connectedComponent g1 v0 == graph
             [(0,"Vertex 0"),(1,"Vertex 1"),(3,"Vertex 3"),(2,"Vertex 2"),(4,"Vertex 4")]
             [(0.5,0,1),(3.7,3,1),(2.2,1,2),(1.3,2,3),(3.2,2,4)],
@@ -55,6 +56,35 @@ tests = [
         isStronglyConnected g4 == True
     ]
 
+testsTreeCover = [  -- Graph.TreeCover tests
+        treeCover g1 == graph
+            [(0,"Vertex 0"),(1,"Vertex 1"),(3,"Vertex 3"),(2,"Vertex 2"),(4,"Vertex 4")]
+            [(0.5,0,1),(3.7,3,1),(2.2,1,2),(3.2,2,4)],
+        treeCover g2 == graph
+            [(5,"Vertex 5"),(6,"Vertex 6"),(8,"Vertex 8"),(7,"Vertex 7"),(9,"Vertex 9")]
+            [(0.5,5,6),(1.5,5,8),(2.2,7,6),(3.2,7,9)],
+
+        minTreeCover g2 == graph
+            [(5,"Vertex 5"),(6,"Vertex 6"),(7,"Vertex 7"),(8,"Vertex 8"),(9,"Vertex 9")]
+            [(0.5,5,6),(1.3,7,8),(1.5,5,8),(3.2,7,9)],
+        minTreeCover g4 == graph
+            [(0,"Vertex 0"),(1,"Vertex 1"),(2,"Vertex 2")]
+            [(0.5,0,1),(1.3,2,0)],
+        
+        minForestCover g3 == [
+            graph
+                [(5,"Vertex 5"),(6,"Vertex 6"),(8,"Vertex 8"),(7,"Vertex 7"),(9,"Vertex 9")]
+                [(0.5,5,6),(1.3,7,8),(1.5,5,8),(3.2,7,9)],
+            graph
+                [(0,"Vertex 0"),(1,"Vertex 1"),(3,"Vertex 3"),(2,"Vertex 2"),(4,"Vertex 4")]
+                [(0.5,0,1),(1.3,2,3),(2.2,1,2),(3.2,2,4)]
+        ]
+    ]
+
+
 result
     | and tests = "Everything correct!"
-    | otherwise = show (length [b | b<-tests, b==False]) ++ " failed tests!"
+    | otherwise = show nFailed ++ " failed test" ++ if nFailed==1 then "!" else "s!"
+    where
+        tests = concat [testsGraph, testsSamples, testsConnection, testsTreeCover]
+        nFailed = length [b | b<-tests, b==False]
