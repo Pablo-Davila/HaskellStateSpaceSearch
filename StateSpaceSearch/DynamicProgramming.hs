3e6011f2950191ae97a363a58b22aecb8fa6e2e4
Pablo Dávila Herrero <pablodavila314@gmail.com>
2022-01-21 18:23:56 +0100

    Add DP algorithm without reduction

--- StateSpaceSearch/DynamicProgramming.hs
+++ StateSpaceSearch/DynamicProgramming.hs
@@ -1,23 +1,37 @@
 
 module StateSpaceSearch.DynamicProgramming (
     dynamicpMin,
+    dynamicpRedMin,
     reconstructSolState
 ) where
 
 
--- Dynamic programming generic algorithm --
+-- Dynamic programming generic algorithms --
 
-dynamicpMin :: (Ord v) => e -> (e -> [a]) -> (e -> a -> e) -> (e -> v) -> (e -> Bool) -> ([a], v)
-dynamicpMin e@estado generarAcciones aplicarAccion f_opt esEstadoFinal
-    | esEstadoFinal e = ([], f_opt e)
+-- Fully generic dynamic programming algorithm
+dynamicpMin :: (Ord v) => e -> (e -> [a]) -> (e -> a -> [e]) -> (e -> v) -> (e -> Bool) -> ([v] -> v) -> v
+dynamicpMin e@estado generarAcciones aplicarAccion fOpt esEstadoFinal solucionParcial
+    | esEstadoFinal e = fOpt e
+    | otherwise = minimum ls
+    where
+        resSubproblemas es = [dynamicpMin e2 generarAcciones aplicarAccion fOpt esEstadoFinal solucionParcial | e2<-es]
+        next a = solucionParcial $ resSubproblemas $ aplicarAccion e a
+        ls = fOpt e:[next a | a<-generarAcciones e]
+
+-- Dynamic programming algorithm with reduction
+dynamicpRedMin :: (Ord v) => e -> (e -> [a]) -> (e -> a -> e) -> (e -> v) -> (e -> Bool) -> ([a], v)
+dynamicpRedMin e@estado generarAcciones aplicarAccion fOpt esEstadoFinal
+    | esEstadoFinal e = ([], fOpt e)
     | otherwise = customMinimumBy snd ls
     where
-        next ac = dynamicpMin (aplicarAccion e ac) generarAcciones aplicarAccion f_opt esEstadoFinal
-        ls = ([], f_opt e):[
-                (a:(fst (next a)), snd (next a))
+        next a = dynamicpRedMin (aplicarAccion e a) generarAcciones aplicarAccion fOpt esEstadoFinal
+        addAction a (as,v) = (a:as, v)
+        ls = ([], fOpt e):[
+                addAction a (next a)
                 | a<-generarAcciones e
             ]
 
+-- Function used to reconstruct the final solution state in a reduction problem applying a sequence of actions
 reconstructSolState :: (e -> a -> e) -> (e -> Bool) -> e -> [a] -> e
 reconstructSolState aplicarAccion esEstadoFinal e@estado acciones
     | esEstadoFinal e = e
